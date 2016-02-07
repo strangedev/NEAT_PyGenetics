d1522b7d390c41b8049fa65c1ebc425a64f026c7
strangedev <strange.dev@gmail.com>
2016-02-07 23:05:34 +0100

    Added directories in GenomeStructures

---
+++ NEAT/GenomeStructures/AnalysisStructure/AnalysisStructure.py
@@ -0,0 +1,8 @@
+from NEAT.GenomeStructures.SimulationStructure import SimulationStructure
+from NEAT.GenomeStructures.SimulationStructure import SimulationNodes
+from NEAT.GenomeStructures.StorageStructures import
+
+class AnalysisStructure(object):
+
+    def __init__(self):
+        pass
