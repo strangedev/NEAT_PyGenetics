44bef9d3c4a77119fa664bb7f0a3b217103f3867
yeldiRium <yeldiR@web.de>
2016-02-09 20:14:49 +0100

    implement SimulationNodes

--- NEAT/GenomeStructures/SimulationStructure/SimulationNodes.py
+++ NEAT/GenomeStructures/SimulationStructure/SimulationNodes.py
@@ -1,20 +1,132 @@
+from typing import Dict, List, Iterable, Tuple
+
+
 class Node(object):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, initial_value: float = 0) -> None:
+        """
+        Successors are stored in a set to prevent adding the same successor mul-
+        tiple times. Weights are stored separately and are accessed with the
+        successor as the key.
+        :param initial_value: Optional initial value. May or may not be over-
+         written when starting a simulation-step.
+        :return:
+        """
+        self.successors = []  # type: List[Node]
+        self.weights = {}  # type: Dict[Node, float]
+        self.initial_value = initial_value
+        self.value = initial_value
 
     def add_successor(self,
                       successor_node: 'Node',
                       weight: float
                       ) -> None:
-        pass
+        """
+        Adds a successor_node to the list of stored successors.
+        :param successor_node: A new node to be added as a successor
+        :param weight: The weight of the edge to the new node
+        :raises: Exception, if the given successor_node already exists in the
+         stored list of successors.
+        """
+        if successor_node not in self.successors:
+            self.successors.append(successor_node)
+            self.weights[successor_node] = weight
+        else:
+            raise Exception("The given successor_node "
+                            .join(str(successor_node))
+                            .join(" is already present in Node ")
+                            .join(str(self)).join("."))
+
+    def add_successors(self,
+                       successor_nodes: Iterable[Tuple['Node', float]]) -> None:
+        """
+        Adds multiple successor_nodes to the list.
+        :param successor_nodes:
+        :return:
+        """
+        for successor_node, weight in successor_nodes:
+            self.add_successor(successor_node, weight)
 
     def fire(self) -> None:
-        pass
+        """
+        Adds the currently stored value multiplied with the specific weights
+        to each successor.
+        Some sort of transformation function should be applied here.
+        :return:
+        """
+        for successor in self.successors:
+            successor.add_value(self.value * self.weights[successor])
 
     def reset(self) -> None:
-        pass
+        """
+        Resets the value to the initial_value.
+        :return:
+        """
+        self.value = self.initial_value
+
+    def add_value(self, value: float) -> None:
+        """
+        Adds the given value to the currently stored value.
+        :param value: The value to be added.
+        :return:
+        """
+        self.value += value
 
 
 class CycleNode(Node):
+    def __init__(self, initial_value: float):
+        """
+        Unlike in Node here the initial_value is mandatory. It is used for the
+        first step of firing the cycle edges.
+        :param initial_value:
+        :return:
+        """
+        super().__init__(initial_value)
+        self.cycle_successors = []  # type: List[Node]
+        self.cycle_weights = {}  # type: Dict[Node, float]
+        self.memory_value = initial_value
+
+    def add_cycle_successor(self, successor_node: Node, weight: float) -> None:
+        """
+        Adds a cycle successor to the list. Throws an Exception otherwise.
+        :param successor_node:
+        :param weight:
+        :raises: Exception, if the given successor_node already exists in the
+         stored list of successors.
+        :return:
+        """
+        if successor_node not in self.cycle_successors:
+            self.cycle_successors.append(successor_node)
+            self.cycle_weights[successor_node] = weight
+        else:
+            raise Exception("The given cycle_successor_node "
+                            .join(str(successor_node))
+                            .join(" is already present in CycleNode ")
+                            .join(str(self)).join("."))
+
+    def add_cycle_successors(
+            self,
+            successor_nodes: Iterable[Tuple[Node, float]]
+    ) -> None:
+        """
+        Adds multiple successor_nodes to the list.
+        :param successor_nodes:
+        :return:
+        """
+        for successor_node, weight in successor_nodes:
+            self.add_cycle_successor(successor_node, weight)
+
     def fire_cycles(self) -> None:
-        pass
+        """
+        Adds the last stored value multiplied with the specific weights to all
+        cycle closing successors.
+        Serves as some kind of short time memory. (Use to be determined)
+        :return:
+        """
+        for cycle_successor in self.cycle_successors:
+            cycle_successor.add_value(
+                self.memory_value * self.cycle_weights[cycle_successor]
+            )
+
+    def reset(self) -> None:
+        self.memory_value = self.value
+        self.value = self.initial_value
