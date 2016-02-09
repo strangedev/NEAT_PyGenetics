ddffd4fd5fb8ddf94f3737d8ab2e73078dfc9b3d
yeldiRium <yeldiR@web.de>
2016-02-09 16:55:41 +0100

    update SimulationGenome: adjust typehinting and formatting

--- NEAT/GenomeStructures/SimulationStructure/SimulationGenome.py
+++ NEAT/GenomeStructures/SimulationStructure/SimulationGenome.py
@@ -1,17 +1,21 @@
-from typing import Iterable, Tuple, Generic
+from typing import Iterable, Tuple, Generic, Dict
 
 from NEAT.GenomeStructures.TH_GenomeStructure import GenomeStructure
+from NEAT.GenomeStructures.SimulationStructure.SimulationNodes import Node
+from NEAT.GenomeStructures.SimulationStructure.SimulationNodes import CycleNode
+
 
 class SimulationGenome(Generic[GenomeStructure]):
     def __init__(
             self,
-            id: int,
-            input_layer=None,
-            output_layer=None,
-            hidden_layer=None,
-            cycle_nodes=None
+            genome_id: int,
+            input_layer: Dict[str: Node],
+            output_layer: Dict[str: Node],
+            hidden_layer: Iterable[Node],
+            cycle_nodes: Iterable[CycleNode]
     ) -> None:
         """
+        :param genome_id: The genome's global id.
         :param input_layer: Dict of input label:nodes.
         :param output_layer: Dict of output label:nodes.
         :param hidden_layer: List of hidden nodes in topological order.
@@ -19,11 +23,11 @@ class SimulationGenome(Generic[GenomeStructure]):
          Subset of hidden_layer.
         :return SimulationGenome:
         """
-        self.id = id
-        self.input_layer = input_layer or {} # type: dict
-        self.output_layer = output_layer or {} # type: dict
-        self.hidden_layer = hidden_layer or [] # type: list
-        self.cycle_nodes = cycle_nodes or [] # type: list
+        self.genome_id = genome_id
+        self.input_layer = input_layer if input_layer else {}
+        self.output_layer = output_layer if output_layer else {}
+        self.hidden_layer = hidden_layer if hidden_layer else []
+        self.cycle_nodes = cycle_nodes if cycle_nodes else []
 
     def set_input(
             self,
@@ -31,22 +35,22 @@ class SimulationGenome(Generic[GenomeStructure]):
     ) -> None:
 
         for label, value in inputs:
-            self.input_layer[label].set_value(value)
+            self.input_layer[label].value = value
 
     @property
-    def output(self) -> dict:
+    def output(self) -> Dict[str: float]:
         """
         :return: Dict of output nodes' label:value
         """
-        return {label: node.get_value() for label, node in self.output_layer}
+        return {label: node.value for label, node in self.output_layer}
 
     def calculate_step(
             self,
             inputs: Iterable[Tuple[str, float]]
-    ) -> dict:
+    ) -> Dict[str: float]:
         """
         :param inputs: List of tuples of the form (node_label, value)
-        :return: None
+        :return: Dict of output node labels and their current values.
         """
         self.set_input(inputs)
 
