4d9ceb87c08d456c7387fd1d6839bd9f124bfd35
strangedev <strange.dev@gmail.com>
2016-02-10 21:00:44 +0100

    Finished AnalysisGenome, yet untested.

---
+++ NEAT/Tests/AnalysisGenomeTests/test_analysisGenome.py
@@ -0,0 +1,36 @@
+from unittest import TestCase
+
+
+class TestAnalysisGenome(TestCase):
+  def test__add_node(self):
+    self.fail()
+
+  def test__add_edge(self):
+    self.fail()
+
+  def test__add_cycle_node(self):
+    self.fail()
+
+  def test__add_cycle_edge(self):
+    self.fail()
+
+  def test_init_from_storage_structure(self):
+    self.fail()
+
+  def test_analyse(self):
+    self.fail()
+
+  def test__reset_analysis(self):
+    self.fail()
+
+  def test__set_working_graph(self):
+    self.fail()
+
+  def test__dfs(self):
+    self.fail()
+
+  def test__dfs_visit(self):
+    self.fail()
+
+  def test_result(self):
+    self.fail()
