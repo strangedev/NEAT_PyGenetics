4dbb7350ed06280fb5a686c297ccd65a5009c739
ohaguro <ohaguro6@gmail.com>
2016-02-24 17:06:42 +0100

    Update test_analysisGenome: Implement test methods for _add_input- and -output_nodes, call _add_-functions with node_id instead of label

--- NEAT/Tests/AnalysisGenomeTests/test_analysisGenome.py
+++ NEAT/Tests/AnalysisGenomeTests/test_analysisGenome.py
@@ -1,30 +1,53 @@
 from unittest import TestCase
 from NEAT.GenomeStructures.AnalysisStructure import AnalysisGenome
+from NEAT.Tests.MockClasses.MockGeneRepository import MockGeneRepository
+from NEAT.GenomeStructures.StorageStructure.StorageGenome import StorageGenome
 
 
 class TestAnalysisGenome(TestCase):
 
     def test__add_node(self):
 
-        ana = AnalysisGenome.AnalysisGenome()
+        self.mock_gene_repository = MockGeneRepository()
+        self.storage_genome = StorageGenome()
+        ana = AnalysisGenome.AnalysisGenome(self.mock_gene_repository, self.storage_genome)
         self.assertSetEqual(ana._nodes, set({}))
 
-        ana._add_node("A")
-        self.assertSetEqual(ana._nodes, {"A"})
+        ana._add_node(6)
+        self.assertSetEqual(ana._nodes, {6})
 
-        ana._add_node("A")
-        self.assertSetEqual(ana._nodes, {"A"})
+        ana._add_node(10)
+        self.assertSetEqual(ana._nodes, {6, 10})
 
     def test__add_edge(self):
 
         ana = AnalysisGenome.AnalysisGenome()
         self.assertDictEqual(ana._edges, {})
 
-        ana._add_node("A")
-        ana._add_node("B")
+        ana._add_node(1)
+        ana._add_node(2)
 
-        ana._add_edge("A", "B")
-        self.assertDictEqual(ana._edges, {"A": ["B"]})
+        ana._add_edge(1, 2)
+        self.assertDictEqual(ana._edges, {1: [2]})
 
     def test_init_from_storage_structure(self):
         self.fail()
+
+    def test__add_input_node(self):
+        self.mock_gene_repository = MockGeneRepository()
+        self.storage_genome = StorageGenome()
+        ana = AnalysisGenome.AnalysisGenome(self.mock_gene_repository, self.storage_genome)
+        self.assertDictEqual(ana._input_nodes, {})
+
+        ana._add_input_node(1, "A")
+        self.assertDictEqual(ana._input_nodes, {"A", 1})
+
+
+    def test_add_output_node(self):
+        self.mock_gene_repository = MockGeneRepository()
+        self.storage_genome = StorageGenome()
+        ana = AnalysisGenome.AnalysisGenome(self.mock_gene_repository, self.storage_genome)
+        self.assertDictEqual(ana._output_nodes, {})
+
+        ana._add_output_node(1, "A")
+        self.assertDictEqual(ana._output_nodes, {"A", 1})
