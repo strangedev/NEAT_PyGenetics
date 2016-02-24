0da7b5439df8aa6e1bd40c29c46a3d4673b902f9
yeldiRium <yeldiRium@web.de>
2016-02-24 18:30:06 +0100

    update AnalysisResult:
    re-add fields edges and cycle_edges

--- NEAT/Analyst/AnalysisResult.py
+++ NEAT/Analyst/AnalysisResult.py
@@ -1,4 +1,4 @@
-from typing import List, Set
+from typing import List, Set, Dict
 
 
 class AnalysisResult(object):
@@ -7,9 +7,11 @@ class AnalysisResult(object):
     """
     def __init__(self) -> None:
         self.disabled_nodes = set({})  # type: Set[int]
+        self.edges = dict({})  # type: Dict[int, List[int]]
         self.topologically_sorted_nodes = []  # type: List[int]
 
         self.cycle_nodes = set({})  # type: Set[int]
+        self.cycle_edges = dict({})  # type: Dict[int, List[int]]
         self.topologically_sorted_cycle_nodes = []  # type: List[int]
 
     def clear(self) -> None:
