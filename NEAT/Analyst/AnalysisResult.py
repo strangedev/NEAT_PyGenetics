0f0851d4d2c4abe9dbf19aee70fe4cde37a74008
yeldiRium <yeldiRium@web.de>
2016-02-24 18:02:46 +0100

    update AnalysisResult: reorder fields

--- NEAT/Analyst/AnalysisResult.py
+++ NEAT/Analyst/AnalysisResult.py
@@ -6,8 +6,8 @@ class AnalysisResult(object):
     Object used to store results of graph analysis.
     """
     def __init__(self) -> None:
-        self.topologically_sorted_nodes = []  # type: List[int]
         self.disabled_nodes = set({})  # type: Set[int]
+        self.topologically_sorted_nodes = []  # type: List[int]
 
         self.cycle_nodes = set({})  # type: Set[int]
         self.topologically_sorted_cycle_nodes = []  # type: List[int]
