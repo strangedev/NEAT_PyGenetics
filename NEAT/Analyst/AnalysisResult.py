3fb1689d9168b81511e4a3a2ee1ec07db6b1fa5a
yeldiRium <yeldiRium@web.de>
2016-02-24 16:13:49 +0100

    update AnalysisResult:
    clean up field to match Issue #4

--- NEAT/Analyst/AnalysisResult.py
+++ NEAT/Analyst/AnalysisResult.py
@@ -1,18 +1,16 @@
-from typing import List, Dict
+from typing import List, Set
 
 
 class AnalysisResult(object):
     """
     Object used to store results of graph analysis.
     """
-
     def __init__(self) -> None:
-        self.nodes = set({})  # type: Set[str]
-        self.cycle_nodes = set({})  # type: Set[str]
-        self.edges = dict({})  # type: Dict[str, List[str]]
-        self.cycle_edges = dict({})  # type: Dict[str, List[str]]
+        self.topologically_sorted_nodes = []  # type: List[int]
+        self.disabled_nodes = set({})  # type: Set[int]
+
+        self.cycle_nodes = set({})  # type: Set[int]
+        self.topologically_sorted_cycle_nodes = []  # type: List[int]
 
     def clear(self) -> None:
         self.__init__()
-
-        
