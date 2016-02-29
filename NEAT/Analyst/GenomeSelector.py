ae10cccfa3f119aa664eab4e13bf13af1ccf5f78
Vivian Franz <vivian.franz4@gmail.com>
2016-02-29 21:41:23 +0100

    Add method to select genomes for discarding in GenomeSelector

---
+++ NEAT/Analyst/GenomeSelector.py
@@ -0,0 +1,31 @@
+from typing import List
+
+from NEAT.GenomeStructures.StorageStructure.StorageGenome import StorageGenome
+from NEAT.Repository.GenomeRepository import GenomeRepository
+from NEAT.Repository.ClusterRepository import ClusterRepository
+
+
+class GenomeSelector(object):
+    def __init__(
+            self,
+            genome_repository: GenomeRepository,
+            cluster_repository: ClusterRepository,
+            selection_parameters
+    ):
+        self.genome_repository = genome_repository
+        self.cluster_repository = cluster_repository
+        self.selection_parameters = selection_parameters
+
+    def select_genomes_for_discarding(self) -> List[StorageGenome]:
+        """
+        discarding_threshold should be in [0 , 1]
+        :return: List of genomes wanted to be discarded
+        """
+        clusters = []
+        for cluster in self.cluster_repository.get_current_clusters():
+            genomes = []
+            for genome in self.genome_repository.get_genomes_in_cluster(cluster.id):
+                genomes.append(genome)
+            genomes.sort(key=lambda genome: genome.fitness)
+            clusters.extend(genomes[:len(genomes)*self.selection_parameters.discarding_threshold])
+        return clusters
