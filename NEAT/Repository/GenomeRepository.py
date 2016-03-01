1cc785638b1a3a0180a45f26412ea9d05f2a5f13
Vivian Franz <vivian.franz4@gmail.com>
2016-03-01 22:29:07 +0100

    Implement GenomeRepository

--- NEAT/Repository/GenomeRepository.py
+++ NEAT/Repository/GenomeRepository.py
@@ -15,23 +15,23 @@ class GenomeRepository(object):
         return storage_genome
 
     def get_current_population(self) -> Iterable[StorageGenome]:
-        pass
+        return self._database_connector.find_many("genomes", {"is_alive": True})
 
     def get_genome_by_id(self, genome_id: ObjectId) -> StorageGenome:
-        pass
+        return self._database_connector.find_one_by_id("genomes", genome_id)
 
     def get_genomes_in_cluster(self, cluster_id: ObjectId) \
             -> Iterable[StorageGenome]:
-        pass
+        return self._database_connector.find_many("genomes", {"cluster": cluster_id})
 
     def insert_genome(self, genome: StorageGenome) -> None:
-        pass
+        self._database_connector.insert_one("genomes", genome)
 
     def insert_genomes(self, genomes: Iterable[StorageGenome]) -> None:
-        pass
+        self._database_connector.insert_many("genomes", genomes)
 
     def update_genome(self, genome: StorageGenome) -> None:
-        pass
+        self._database_connector.update_one("genomes", genome)
 
     def update_genomes(self, genomes: Iterable[StorageGenome]) -> None:
-        pass
+        self._database_connector.update_many("genomes", genomes)
