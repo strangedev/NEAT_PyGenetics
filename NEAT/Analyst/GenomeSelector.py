40002e6cafae7b7ced60985578c551e6fcd7b32a
Vivian Franz <vivian.franz4@gmail.com>
2016-03-01 17:26:44 +0100

    ADD method to discard cluster, FIXBUG float -> int

--- NEAT/Analyst/GenomeSelector.py
+++ NEAT/Analyst/GenomeSelector.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from NEAT.Analyst.Cluster import Cluster
 from NEAT.GenomeStructures.StorageStructure.StorageGenome import StorageGenome
 from NEAT.Repository.GenomeRepository import GenomeRepository
 from NEAT.Repository.ClusterRepository import ClusterRepository
@@ -16,6 +17,13 @@ class GenomeSelector(object):
         self.cluster_repository = cluster_repository
         self.selection_parameters = selection_parameters
 
+    def select_clusters_for_discarding(self) -> List[Cluster]:
+        clusters = []
+        for cluster in self.cluster_repository.get_current_clusters():
+            clusters.append(cluster)
+        clusters.sort(key=lambda cluster: cluster.fitness)
+        return clusters[:int(len(clusters)*self.selection_parameters.discarding_threshold)]
+
     def select_genomes_for_discarding(self) -> List[StorageGenome]:
         """
         discarding_threshold should be in [0 , 1]
@@ -27,5 +35,5 @@ class GenomeSelector(object):
             for genome in self.genome_repository.get_genomes_in_cluster(cluster.id):
                 genomes.append(genome)
             genomes.sort(key=lambda genome: genome.fitness)
-            clusters.extend(genomes[:len(genomes)*self.selection_parameters.discarding_threshold])
+            clusters.extend(genomes[:int(len(genomes)*self.selection_parameters.discarding_threshold)])
         return clusters
