ff1a412c203c99e9752b60841d08c7f7ab4b1006
strangedev <strange.dev@gmail.com>
2016-03-05 16:43:40 +0100

    Added test_GeneRepository

---
+++ NEAT/Repository/test_geneRepository.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from NEAT.Repository.GeneRepository import GeneRepository
+
+
+class TestGeneRepository(TestCase):
+    def test_get_gene_id_for_endpoints(self):
+        self.fail()
+
+    def test_find_connecting_nodes(self):
+        self.fail()
+
+    def test_get_next_node_label(self):
+        self.fail()
+
+    def test_get_node_labels_by_gene_id(self):
+        self.fail()
