367744f0d5d1148e0075ec436ea947473e0c010d
strangedev <strange.dev@gmail.com>
2016-03-08 16:41:33 +0100

    Added docstrings to ProbabilisticTools.weighted_choice()

--- NEAT/Utilities/ProbabilisticTools.py
+++ NEAT/Utilities/ProbabilisticTools.py
@@ -4,6 +4,12 @@ import random
 def weighted_choice(
         weighted_sample: List[Tuple[object, float]]
 ) -> object:
+    """
+    Chooses an object from a list of possible outcomes,
+    possible outcomes are weighted by their probabilities of being chosen.
+    :param weighted_sample: List of Tuples: (outcome, probability)
+    :return: The chosen outcome
+    """
 
     abs_probability = sum([s[1] for s in weighted_sample])
     object_bound_mapper = []
@@ -33,3 +39,16 @@ def weighted_choice(
             return interval[0]
 
     return object_bound_mapper[-1][0]
+
+def weighted_choice_range(
+        weighted_sample: List[Tuple[object, float]],
+        count: int
+) -> List[object]:
+    """
+    Chooses a number of objects from a list of possible outcomes,
+    possible outcomes are weighted by their probabilities of being chosen.
+    :param weighted_sample: List of Tuples: (outcome, probability)
+    :param count: Amount of outcomes to choose
+    :return: The chosen outcomes
+    """
+    pass
