45d35ee8b9bd6eb6898abd1c691a28c994fef965
Vivian Franz <vivian.franz4@gmail.com>
2016-03-08 01:33:10 +0100

    ADD select_genomes_for_breeding, _genome_for_mutation, get_genomes_in_cluster_from_cluster; Selection made by fitness

--- NEAT/Analyst/GenomeSelector.py
+++ NEAT/Analyst/GenomeSelector.py
@@ -19,9 +19,53 @@ class GenomeSelector(object):
         :param selection_parameters: saved in selection.conf
         :return:
         """
+
         self.genome_repository = genome_repository
         self.cluster_repository = cluster_repository
         self.selection_parameters = selection_parameters
+
+    def get_genomes_in_cluster_from_cluster(self, begin: float, ending: float):
+        """
+        Selects and Area of genomes in Cluster given by input
+        :param begin: float where to begin to select genomes
+        :param ending: float where to end select genomes
+        :return: tuple([[StorageGenome]], [StorageGenome])
+        """
+        clusters = []
+        genomes_result = []
+        for cluster in self.cluster_repository:
+            genomes = []
+            for genome in self.genome_repository(cluster._id):
+                genomes.append(genome)
+            genomes.sort(key=lambda g: g.fitness)
+            start = int(len(genomes)*begin)
+            end = int(len(genomes)*ending)
+            genomes_result.append(genomes[start:end])
+            clusters.extend(genomes[start:end])
+
+        return clusters, genomes_result
+
+    def select_genomes_for_breeding(self) -> tuple(StorageGenome):
+        """
+        Selects genomes for breeding currently best two from all Clusters
+        :return: tuple(Storage)
+        """
+        result = self.get_genomes_in_cluster_from_cluster(0, 1)[1]
+        result.sort(key=lambda genome: genome.fitness)
+        return tuple(result[len(result)-2], len(result)-1)
+
+    def select_genome_for_mutation(self) -> StorageGenome:
+        """
+        Selects genome for mutation currently the best from all Clusters
+        :return: StorageGenome the most fit
+        """
+        return self.select_genomes_for_breeding()[1]
+
+    def select_clusters_for_combination(self) -> tuple(Cluster):
+        pass # Todo
+
+    def select_cluster_combination(self, cluster1: Cluster, cluster2: Cluster) -> List[tuple(StorageGenome)]:
+        pass # Todo
 
     def select_clusters_for_discarding(self) -> List[Cluster]:
         """
@@ -40,11 +84,7 @@ class GenomeSelector(object):
         select "discarding_by_genome_fitness" percentage (in every Cluster) of genomes sorted ascending by fitness
         :return: List of Genomes wanted to be discarded
         """
-        clusters = []
-        for cluster in self.cluster_repository.get_current_clusters():
-            genomes = []
-            for genome in self.genome_repository.get_genomes_in_cluster(cluster._id):
-                genomes.append(genome)
-            genomes.sort(key=lambda genome: genome.fitness)
-            clusters.extend(genomes[:int(len(genomes)*self.selection_parameters["discarding_by_genome_fitness"])])
+        self._set_selection(0.0,self.selection_parameters["discarding_by_genome_fitness"])
+        clusters = self.get_genomes_in_cluster_from_cluster()[1]
+        self._reset()
         return clusters
