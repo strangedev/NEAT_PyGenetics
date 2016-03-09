ec95a332dc7e91f932b4e117517e851e5d61140d
strangedev <strange.dev@gmail.com>
2016-03-09 18:48:48 +0100

    Added CommandTranscoder, Implemented BaseCommand

--- NEAT/Networking/Server/CommandTranscoder.py
+++ NEAT/Networking/Server/CommandTranscoder.py
@@ -1,11 +1,23 @@
 from NEAT.Networking.Commands.BaseCommand import BaseCommand
 
+type_class_map = dict(
+    {
+        "BaseCommand": BaseCommand
+    }
+)
+
 class CommandTranscoder(object):
 
     @staticmethod
     def encode_command(command: BaseCommand):
-        pass
+        return command.as_dict()
 
     @staticmethod
-    def decode_command(commmand: BaseCommand):
-        pass
+    def decode_command(dictionary: dict):
+        if not "_type" in dictionary.keys():
+            return None
+        command_class = type_class_map[dictionary["_type"]]
+        command = command_class()
+
+        command.from_dict(dictionary)
+        return command
