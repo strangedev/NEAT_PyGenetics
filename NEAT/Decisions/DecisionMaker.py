e2eecf7fecea4f6be3353f66a21ab3fa13db2129
Vivian Franz <vivian.franz4@gmail.com>
2016-03-11 13:13:27 +0100

    FIX light warnings, like PEP it wants

--- NEAT/Decisions/DecisionMaker.py
+++ NEAT/Decisions/DecisionMaker.py
@@ -3,6 +3,7 @@ Makes decisions lol
 """
 import math
 
+
 class DecisionMaker(object):
 
     def __init__(self, decision_making_parameters):
@@ -22,18 +23,16 @@ class DecisionMaker(object):
             self._time,
             self._decision_making_parameters["mutation_cutoff_point"]
         )
-        return (base_percentage + (1 - base_percentage) * additional_percentage)
+        return base_percentage + (1 - base_percentage) * additional_percentage
 
     @property
     def inter_cluster_breeding_time(self):
         return (
             self._time %
-            self._decision_making_parameters["inter_cluster_breeding_interval"]
-            == 0
+            self._decision_making_parameters["inter_cluster_breeding_interval"] == 0
         )
 
     def _cutoff_function(self, x, cutoff_point):
         return (1 / math.exp(x /
-                             self._decision_making_parameters["cutoff_point_scalar"]
-                             * math.log(cutoff_point)))
+                             self._decision_making_parameters["cutoff_point_scalar"] * math.log(cutoff_point)))
 
---
+++ NEAT/Tests/Decisions/test_decisionMaker.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class TestDecisionMaker(TestCase):
+    def test_advance_time(self):
+        self.fail()
+
+    def test_reset_time(self):
+        self.fail()
+
+    def test_mutation_percentage(self):
+        self.fail()
+
+    def test_inter_cluster_breeding_time(self):
+        self.fail()
+
+    def test__cutoff_function(self):
+        self.fail()
