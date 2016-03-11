dc2322a0c34517114ee2e0f09f2257e048498a5b
Vivian Franz <vivian.franz4@gmail.com>
2016-03-11 12:51:46 +0100

    FIX exceptionTests in test_transformator

--- NEAT/Tests/RepositoryTests/test_transformator.py
+++ NEAT/Tests/RepositoryTests/test_transformator.py
@@ -19,35 +19,46 @@ class TestTransformator(TestCase):
     def test_encode_StorageGenome(self):
         storage_genome = StorageGenome()
         storage_dict = copy.deepcopy(storage_genome.__dict__)
-        analysis_dict = Transformator.encode_AnalysisResult()
+        analysis_dict = Transformator.encode_AnalysisResult(storage_dict.__getitem__('analysis_result'))
         storage_dict.__setitem__('analysis_result', analysis_dict)
         storage_dict.__setitem__('_type', 'StorageGenome')
         self.assertDictEqual(storage_dict, Transformator.encode_StorageGenome(storage_genome))
 
     def test_decode_AnalysisResult(self):
         analysis_result = AnalysisResult()
-        dict = analysis_result.__dict__
-        dict.__setitem__('_type', 'AnalysisResult')
-        self.assertTrue(analysis_result.__eq__(Transformator.decode_AnalysisResult(dict)))
+        dictionary = analysis_result.__dict__
+        dictionary.__setitem__('_type', 'AnalysisResult')
+        self.assertTrue(analysis_result.__eq__(Transformator.decode_AnalysisResult(dictionary)))
 
     def test_decode_StorageGenome(self):
         storage_genome = StorageGenome()
-        analysis_stor = storage_genome.__dict__.__getitem__('analysis_result')
-        analysis_dict = Transformator.encode_AnalysisResult(analysis_stor)
-        stor = copy.deepcopy(storage_genome.__dict__)
-        stor.__setitem__('analysis_result', analysis_dict)
-        stor.__setitem__('_type', 'StorageGenome')
-        self.assertTrue(storage_genome.__eq__(Transformator.decode_StorageGenome(stor)))
+        analysis_storage = storage_genome.__dict__.__getitem__('analysis_result')
+        analysis_dict = Transformator.encode_AnalysisResult(analysis_storage)
+        storage = copy.deepcopy(storage_genome.__dict__)
+        storage.__setitem__('analysis_result', analysis_dict)
+        storage.__setitem__('_type', 'StorageGenome')
+        self.assertTrue(storage_genome.__eq__(Transformator.decode_StorageGenome(storage)))
 
     @unittest.expectedFailure
     def test_encode_AnalysisResultExpectedKeyError(self):
         e = {}
-        self.assertEqual(None, Transformator.encode_AnalysisResult())
+        analysis_result = AnalysisResult()
+        analysis_result.__dict__ = e
+        self.assertEqual(None, Transformator.encode_AnalysisResult(e))
 
     @unittest.expectedFailure
     def test_decodeAnalysisResultExpectedKeyError(self):
-        self.assertEqual(None, Transformator.decode_AnalysisResult())
+        e = {}
+        self.assertEqual(None, Transformator.decode_AnalysisResult(e))
 
     @unittest.expectedFailure
     def test_encode_StorageGenome(self):
-        self.assertEqual(None, Transformator.encode_StorageGenome())
+        e = {}
+        genome = StorageGenome()
+        genome.__dict__ = e
+        self.assertEqual(None, Transformator.encode_StorageGenome(e))
+
+    @unittest.expectedFailure
+    def test_decodeStorageGenomeExpectedKeyError(self):
+        e = {}
+        self.assertEqual(None, Transformator.decode_StorageGenome(e))
