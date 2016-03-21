920ea63459bafbc3a3884aeca0372226aeec559d
strangedev <strange.dev@gmail.com>
2016-03-22 00:57:01 +0100

    Added missing docstring in Breeder Reformatted Breeder to match PEP-8

--- NEAT/Generator/Breeder.py
+++ NEAT/Generator/Breeder.py
@@ -1,17 +1,16 @@
-from NEAT.GenomeStructures.StorageStructure.StorageGenome import StorageGenome
-from NEAT.Utilities import ProbabilisticTools
-from typing import Dict, Tuple, List
 import random
+from typing import Dict
+
+from NEAT.GenomeStructures.StorageStructure.StorageGenome import StorageGenome
+
 
 class Breeder(object):
-
     def __init__(
             self,
             breeding_parameters: Dict[str, object]
     ) -> None:
 
         self.breeding_parameters = breeding_parameters
-
 
     def breed_genomes(
             self,
@@ -46,11 +45,11 @@ class Breeder(object):
 
         all_gene_ids = list(smaller_genome_gene_ids) + list(bigger_genome_gene_ids)
 
-        matching_gene_ids = [gene_id for gene_id in bigger_genome_gene_ids \
-                          if gene_id in smaller_genome_gene_ids]
+        matching_gene_ids = [gene_id for gene_id in bigger_genome_gene_ids
+                             if gene_id in smaller_genome_gene_ids]
 
-        differing_gene_ids = [gene_id for gene_id in all_gene_ids \
-                           if gene_id not in matching_gene_ids]
+        differing_gene_ids = [gene_id for gene_id in all_gene_ids
+                              if gene_id not in matching_gene_ids]
 
         fitter_genome_table = genome_one.genes \
             if genome_one.fitness > genome_two.fitness \
@@ -60,7 +59,6 @@ class Breeder(object):
 
         # Insert inherited matching genes.
         for gene_id in matching_gene_ids:
-
             parent_genome_table = random.choice(
                 [
                     genome_one.genes,
@@ -79,7 +77,6 @@ class Breeder(object):
         if abs(genome_one.fitness - genome_two.fitness) > \
                 self.breeding_parameters["fitness_difference_threshold"]:
 
-
             for gene_id in differing_gene_ids:
 
                 if gene_id in fitter_genome_table.keys():
@@ -95,8 +92,9 @@ class Breeder(object):
             for gene_id in differing_gene_ids:
 
                 inherit_gene = True \
-                    if random.random() \
-                       < self.breeding_parameters["inherit_randomly_if_same_fitness_probability"] \
+                    if random.random() < self.breeding_parameters[
+                           "inherit_randomly_if_same_fitness_probability"
+                       ] \
                     else False
 
                 if inherit_gene:
@@ -111,10 +109,17 @@ class Breeder(object):
 
                     new_genome.genes[gene_id] = (gene_enabled, gene_weight)
 
-
         return new_genome
 
     def should_gene_be_enabled(self, instance_one, instance_two=None):
+        """
+        Decides whether the inherited variant of a gene should be
+        enabled in the offspring genome based on both parents' instances
+        of the gene.
+        :param instance_one: The first gene instance
+        :param instance_two:  The second gene instance
+        :return: Whether the inherited gene should be enabled
+        """
 
         gene_one_enabled = instance_one[1]
 
@@ -123,9 +128,8 @@ class Breeder(object):
         else:
             gene_two_enabled = instance_two[1]
 
-        if (not gene_one_enabled) or  (not gene_two_enabled):
-
+        if (not gene_one_enabled) or (not gene_two_enabled):
             return False \
-                if random.random() \
-                   < self.breeding_parameters["gene_inherited_as_disabled_probability"] \
+                if random.random() < self.breeding_parameters[
+                    "gene_inherited_as_disabled_probability"] \
                 else True
