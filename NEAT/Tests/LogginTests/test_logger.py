5486267ef07f66b0c0740b38e3d4da4093237e21
Vivian Franz <vivian.franz4@gmail.com>
2016-04-03 15:50:29 +0200

    IMPLEMENT test for test_logger.log() ADD Method to open_log

--- NEAT/Tests/LogginTests/test_logger.py
+++ NEAT/Tests/LogginTests/test_logger.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 import time
 
 import datetime
+from unittest.mock import MagicMock
 
 from NEAT.Logging.Logger import Logger
 
@@ -30,6 +31,7 @@ class TestLogger(TestCase):
     def test_lookup_log_level(self):
         for i in self.log_levels.keys():
             self.assertEqual(self.log_levels[i], self.logger.lookup_log_level(i))
+        self.assertEqual(7, self.logger.lookup_log_level("randomShit"))
 
     def test_lookup_log_level_label(self):
         for i in range(0, 7):
@@ -43,5 +45,11 @@ class TestLogger(TestCase):
                          self.logger.get_timestamp())
 
     def test_log(self):
-        self.fail()
+        self.logger.log("hello", "whatever")
+
+        self.logger.open_log = MagicMock()
+        Logger.get_timestamp = MagicMock(return_value="testing")
+        self.assertEqual("testing", Logger.get_timestamp())
+        self.logger.log("test")
+        self.logger.open_log.assert_called_with("log/./info.log", "log/./log", "testinginfo: test")
 
